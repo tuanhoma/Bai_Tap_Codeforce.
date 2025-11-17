c7a4a002b9949202c972551ab29a68849a0b9c0f
tuanhoma <k7apascaltuanchu@gmail.com>
2025-11-17 22:31:31 +0700

    17/11/2025

---
+++ A_Cloudberry_Jam.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A_Cloudberry_Jam {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            System.out.println(n * 2);
+        }
+    }
+}
